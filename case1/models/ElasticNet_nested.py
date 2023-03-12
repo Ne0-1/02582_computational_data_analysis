9c3a8200f81c2319bc374c96fedcedad125befb8
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2023-03-12 19:13:43 +0100

    Have implemented Elastic net and a nested cross-validation to assess model performance

--- case1/models/ElasticNet_nested.py
+++ case1/models/ElasticNet_nested.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from functions import normalize, centerData
 from sklearn.model_selection import KFold
-from sklearn.linear_model import Lars
+from sklearn.linear_model import ElasticNet
+import warnings
 
 # Sources for approach
 # https://stats.stackexchange.com/questions/254612/how-to-obtain-optimal-hyperparameters-after-nested-cross-validation
@@ -31,7 +32,8 @@ kf1 = KFold(n_splits=CV_outer, random_state=42, shuffle=True)
 
 Err_par = np.zeros(CV_outer)
 Err_test = np.zeros(CV_outer)
-p_opts = np.zeros(CV_outer)
+alpha_opts = np.zeros(CV_outer)
+lambda_opts = np.zeros(CV_outer)
 # From 02450 lecture notes (page 175)
 # Outer cross-validation loop - Estimate generalization error
 for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
@@ -46,12 +48,13 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     kf2 = KFold(n_splits=CV_inner, random_state=42, shuffle=True)
     
     # Define hyper parameter space
-    stop = n-math.ceil(n/CV_inner)
-    K = range(stop)
+    alphas = np.linspace(0, 1, 5)
+    lambdas = np.logspace(-1, 1, 20)
+
 
     # Prepare memory storage
-    Err_tr = np.zeros((CV_inner, len(K)))
-    Err_val = np.zeros((CV_inner, len(K)))
+    Err_tr = np.zeros((CV_inner, len(alphas), len(lambdas)))
+    Err_val = np.zeros((CV_inner, len(alphas), len(lambdas)))
 
     # Inner cross-validation loop - select optimal model
     for j, (inner_train_index, inner_test_index) in enumerate(kf2.split(X_par)):
@@ -72,21 +75,24 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
         y_train = y_train / d
         y_val = y_val / d
 
-        for p in K:
-            model_inner = Lars(n_nonzero_coefs=p, fit_path = False, fit_intercept = False)
-            model_inner.fit(X_train, y_train)
-            
-            # Training predictions
-            beta_inner = model_inner.coef_.ravel()
-            y_hat_train = X_train @ beta_inner  # Don't need intercept due to centered data
-            y_hat_val = X_val @ beta_inner      # Don't need intercept due to centered data
+        with warnings.catch_warnings(): # disable convergence warnings from elastic net
+            warnings.simplefilter("ignore")
+            for a_idx, alpha in enumerate(alphas):
+                for l_idx, lambda_ in enumerate(lambdas):
+                    model_inner = ElasticNet(alpha=lambda_, l1_ratio=alpha)
+                    model_inner.fit(X_train, y_train)
+                    
+                    # Training predictions
+                    beta_inner = model_inner.coef_.ravel()
+                    y_hat_train = X_train @ beta_inner  # Don't need intercept due to centered data
+                    y_hat_val = X_val @ beta_inner      # Don't need intercept due to centered data
 
-            # Compute RMSE for train and validation set
-            Err_tr[j, p] = np.sqrt(((y_train - y_hat_train)**2).mean())
-            Err_val[j, p] = np.sqrt(((y_val - y_hat_val)**2).mean())
+                    # Compute RMSE for train and validation set
+                    Err_tr[j, a_idx, l_idx] = np.sqrt(((y_train - y_hat_train)**2).mean())
+                    Err_val[j, a_idx, l_idx] = np.sqrt(((y_val - y_hat_val)**2).mean())
 
     # Select optimal hyperparameter
-    p_opt = np.argmin(Err_val.mean(axis=0))
+    alpha_opt, lambda_opt = np.where(Err_val.mean(axis=0) == np.min(Err_val.mean(axis=0)))
 
     ############
     # NOT SURE ABOUT THE FOLLOWING!
@@ -108,8 +114,10 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     ###############
 
     # Train optimal model
-    model_outer = Lars(n_nonzero_coefs=p_opt, fit_path = False, fit_intercept = False)
-    model_outer.fit(X_par, y_par)
+    with warnings.catch_warnings(): # disable convergence warnings from elastic net
+        warnings.simplefilter("ignore")
+        model_outer = ElasticNet(alpha=lambda_opt.item(), l1_ratio=alpha_opt.item())
+        model_outer.fit(X_par, y_par)
     
     # Training predictions
     beta_outer = model_outer.coef_.ravel()
@@ -119,7 +127,8 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     # Compute test error
     Err_par[i] = np.sqrt(((y_par - y_hat_par)**2).mean())
     Err_test[i] = np.sqrt(((y_test - y_hat_test)**2).mean())
-    p_opts[i] = p_opt
+    alpha_opts[i] = alpha_opt
+    lambda_opts[i] = lambda_opt
 
 # Compute estimate of generalization error
 E_gen = Err_test.mean()
@@ -127,38 +136,38 @@ E_gen = Err_test.mean()
 # Results:
 print('Computed estimated test error E_test[i]: ', Err_test)
 print('')
-print('Correspondind optimal hyperparamter p*: ', p_opts)
+print('Correspondind optimal hyperparamter p*: ', alpha_opts, lambda_opts)
 print('')
 print('Estimated generalization error: ')
 print(E_gen)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), dpi=100)
+#fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), dpi=100)
 
-ax1.plot(K, Err_tr.T, ':', alpha=0.5)
-ax1.plot(Err_tr.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
-ax1.set_title('Training error for each fold')
-ax1.legend()
-ax1.set_xlabel(r"non-zero $\beta$")
-ax1.set_ylabel('RMSE')
+#ax1.plot(K, Err_tr.T, ':', alpha=0.5)
+#ax1.plot(Err_tr.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
+#ax1.set_title('Training error for each fold')
+#ax1.legend()
+#ax1.set_xlabel(r"non-zero $\beta$")
+#ax1.set_ylabel('RMSE')
 
-ax2.plot(K, Err_val.T, ':', alpha=0.5)
-ax2.plot(Err_val.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
+#ax2.plot(K, Err_val.T, ':', alpha=0.5)
+#ax2.plot(Err_val.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
 #ax2.axvline(p_opt, label=r"$p^{*}$", linestyle='dashed', c='k', alpha=0.75)
-[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
-ax2.set_title('Validation error for each fold')
-ax2.set_yscale('log')
-ax2.legend()
-ax2.set_xlabel(r"non-zero $\beta$")
-ax2.set_ylabel('RMSE')
-plt.show()
+#[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
+#ax2.set_title('Validation error for each fold')
+#ax2.set_yscale('log')
+#ax2.legend()
+#ax2.set_xlabel(r"non-zero $\beta$")
+#ax2.set_ylabel('RMSE')
+#plt.show()
 
-fig, ax = plt.subplots(figsize=(15,5), dpi=100)
-ax.plot(K, Err_tr.mean(axis=0), c='darkblue', label='Average training error')
-ax.plot(K, Err_val.mean(axis=0), c='firebrick', label='Average validation error')
+#fig, ax = plt.subplots(figsize=(15,5), dpi=100)
+#ax.plot(K, Err_tr.mean(axis=0), c='darkblue', label='Average training error')
+#ax.plot(K, Err_val.mean(axis=0), c='firebrick', label='Average validation error')
 #ax.axvline(p_opt, label=r"$p^{*}$", linestyle='dashed', c='k', alpha=0.75)
-[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
-ax.set_yscale('log')
-ax.legend()
-ax.set_xlabel(r"non-zero $\beta$")
-ax.set_ylabel('RMSE')
-plt.show()
+#[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
+#ax.set_yscale('log')
+#ax.legend()
+#ax.set_xlabel(r"non-zero $\beta$")
+#ax.set_ylabel('RMSE')
+#plt.show()
