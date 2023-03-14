Added new models and fixed data leakage bug caused by imputing values prior to cross-validation splits

case1/models/LarsLasso.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import KFold
 from sklearn.linear_model import Lars
 
 #TODO: Maybe change to nested cross-validation
+#TODO: All methods should be evaluated on the same splits!
 
 def setup_mpl():
     mpl.rcParams['font.family'] = 'Times New Roman'
@@ -26,7 +27,7 @@ CV = 5
 kf = KFold(n_splits=CV)
  
 stop = n-math.ceil(n/CV)
-K = range(stop)
+K = range(stop)  # TODO: assert this part, why do we use this appraoch?
 
 Err_tr = np.zeros((CV,len(K)))
 Err_val = np.zeros((CV, len(K)))

case1/models/Lars_nested.py
@@ -19,14 +19,12 @@ def setup_mpl():
 y = np.loadtxt('case1/data/response.txt')
 X = np.loadtxt('case1/data/independent.txt')
 
-#Temp NaN solution, replace nan with column mean
-X = np.where(np.isnan(X), np.ma.array(X, mask=np.isnan(X)).mean(axis=0), X) 
 
 (n, p) = X.shape
 
 # Define outer cross validation split
 CV_outer = 10
-CV_inner = 10
+CV_inner = 5
 kf1 = KFold(n_splits=CV_outer, random_state=42, shuffle=True)
 
 Err_par = np.zeros(CV_outer)
@@ -56,21 +54,26 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     # Inner cross-validation loop - select optimal model
     for j, (inner_train_index, inner_test_index) in enumerate(kf2.split(X_par)):
             
-        # Center data, notice test is centered using training mean
-        y_train, mu = centerData(y_par[inner_train_index])
-        y_val = y_par[inner_test_index] - mu
+        X_train = X_par[inner_train_index, :]
+        y_train = y_par[inner_train_index]
 
-        X_train, mu = centerData(X_par[inner_train_index, :])
-        X_val = X_par[inner_test_index,:] - mu
+        X_val = X_par[inner_test_index, :]
+        y_val = y_par[inner_test_index]
+
+        # Imputing missing values
+        X_train = np.where(np.isnan(X_train), np.ma.array(X_train, mask=np.isnan(X_train)).mean(axis=0), X_train)
+        X_val = np.where(np.isnan(X_val), np.ma.array(X_val, mask=np.isnan(X_val)).mean(axis=0), X_val)
+            
+        # Center data, notice test is centered using training mean
+        y_train, mu = centerData(y_train)
+        y_val = y_val - mu
+
+        X_train, mu = centerData(X_train)
+        X_val = X_val - mu
 
         # Normalize val using train
         X_train, d = normalize(X_train)
         X_val = X_val / d
-        
-        ## I think we should normalize y_train + y_val even though this is not done in the exercises
-        d = np.linalg.norm(y_train, axis=0, ord=2) 
-        y_train = y_train / d
-        y_val = y_val / d
 
         for p in K:
             model_inner = Lars(n_nonzero_coefs=p, fit_path = False, fit_intercept = False)
@@ -88,8 +91,10 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     # Select optimal hyperparameter
     p_opt = np.argmin(Err_val.mean(axis=0))
 
-    ############
-    # NOT SURE ABOUT THE FOLLOWING!
+    # Imputing missing values
+    X_par = np.where(np.isnan(X_par), np.ma.array(X_par, mask=np.isnan(X_par)).mean(axis=0), X_par)
+    X_test = np.where(np.isnan(X_test), np.ma.array(X_test, mask=np.isnan(X_test)).mean(axis=0), X_test)
+
     # Center data, notice test is centered using training mean
     y_par, mu = centerData(y_par)
     y_test = y_test - mu
@@ -100,12 +105,6 @@ for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
     # Normalize val using train
     X_par, d = normalize(X_par)
     X_test = X_test / d
-    
-    # I think we should normalize even though this is not done in the exercises (Maybe the coeffs will handle it if we don't?)
-    d = np.linalg.norm(y_par, axis=0, ord=2) 
-    y_par = y_par / d
-    y_test = y_test / d
-    ###############
 
     # Train optimal model
     model_outer = Lars(n_nonzero_coefs=p_opt, fit_path = False, fit_intercept = False)
@@ -144,7 +143,7 @@ ax1.set_ylabel('RMSE')
 ax2.plot(K, Err_val.T, ':', alpha=0.5)
 ax2.plot(Err_val.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
 #ax2.axvline(p_opt, label=r"$p^{*}$", linestyle='dashed', c='k', alpha=0.75)
-[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
+#[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
 ax2.set_title('Validation error for each fold')
 ax2.set_yscale('log')
 ax2.legend()

case1/models/ridge_nested.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from functions import normalize, centerData
 from sklearn.model_selection import KFold
-from sklearn.linear_model import ElasticNet
-import warnings
-from tqdm import tqdm
+from sklearn.linear_model import Ridge
 
 # Sources for approach
 # https://stats.stackexchange.com/questions/254612/how-to-obtain-optimal-hyperparameters-after-nested-cross-validation
@@ -30,11 +28,10 @@ kf1 = KFold(n_splits=CV_outer, random_state=42, shuffle=True)
 
 Err_par = np.zeros(CV_outer)
 Err_test = np.zeros(CV_outer)
-alpha_opts = np.zeros(CV_outer)
 lambda_opts = np.zeros(CV_outer)
 # From 02450 lecture notes (page 175)
 # Outer cross-validation loop - Estimate generalization error
-for i, (outer_train_index, outer_test_index) in tqdm(enumerate(kf1.split(X))):
+for i, (outer_train_index, outer_test_index) in enumerate(kf1.split(X)):
 
     y_par = y[outer_train_index]
     X_par = X[outer_train_index, :]
@@ -46,17 +43,15 @@ for i, (outer_train_index, outer_test_index) in tqdm(enumerate(kf1.split(X))):
     kf2 = KFold(n_splits=CV_inner, random_state=42, shuffle=True)
     
     # Define hyper parameter space
-    alphas = np.arange(0, 1, 0.01)
-    lambdas = np.logspace(-5, 100)
-
+    lambdas = np.logspace(-5, 1)
 
     # Prepare memory storage
-    Err_tr = np.zeros((CV_inner, len(alphas), len(lambdas)))
-    Err_val = np.zeros((CV_inner, len(alphas), len(lambdas)))
+    Err_tr = np.zeros((CV_inner, len(lambdas)))
+    Err_val = np.zeros((CV_inner, len(lambdas)))
 
     # Inner cross-validation loop - select optimal model
     for j, (inner_train_index, inner_test_index) in enumerate(kf2.split(X_par)):
-            
+        
         X_train = X_par[inner_train_index, :]
         y_train = y_par[inner_train_index]
 
@@ -78,26 +73,21 @@ for i, (outer_train_index, outer_test_index) in tqdm(enumerate(kf1.split(X))):
         X_train, d = normalize(X_train)
         X_val = X_val / d
 
-        with warnings.catch_warnings(): # disable convergence warnings from elastic net
-            warnings.simplefilter("ignore")
-            for a_idx, alpha in enumerate(alphas):
-                for l_idx, lambda_ in enumerate(lambdas):
-                    model_inner = ElasticNet(alpha=lambda_, l1_ratio=alpha)
-                    model_inner.fit(X_train, y_train)
-                    
-                    # Training predictions
-                    beta_inner = model_inner.coef_.ravel()
-                    y_hat_train = X_train @ beta_inner  # Don't need intercept due to centered data
-                    y_hat_val = X_val @ beta_inner      # Don't need intercept due to centered data
+        for lambda_idx, lambda_ in enumerate(lambdas):
+            model_inner = Ridge(alpha=lambda_, fit_intercept = False, tol=1e-4)
+            model_inner.fit(X_train, y_train)
+            
+            # Training predictions
+            beta_inner = model_inner.coef_.ravel()
+            y_hat_train = X_train @ beta_inner  # Don't need intercept due to centered data
+            y_hat_val = X_val @ beta_inner      # Don't need intercept due to centered data
 
-                    # Compute RMSE for train and validation set
-                    Err_tr[j, a_idx, l_idx] = np.sqrt(((y_train - y_hat_train)**2).mean())
-                    Err_val[j, a_idx, l_idx] = np.sqrt(((y_val - y_hat_val)**2).mean())
+            # Compute RMSE for train and validation set
+            Err_tr[j, lambda_idx] = np.sqrt(((y_train - y_hat_train)**2).mean())
+            Err_val[j, lambda_idx] = np.sqrt(((y_val - y_hat_val)**2).mean())
 
     # Select optimal hyperparameter
-    alpha_opt_idx, lambda_opt_idx = np.where(Err_val.mean(axis=0) == np.min(Err_val.mean(axis=0)))
-    alpha_opt = alphas[alpha_opt_idx]
-    lambda_opt = lambdas[lambda_opt_idx]
+    lambda_opt = lambdas[np.argmin(Err_val.mean(axis=0))]
 
     # Imputing missing values
     X_par = np.where(np.isnan(X_par), np.ma.array(X_par, mask=np.isnan(X_par)).mean(axis=0), X_par)
@@ -113,12 +103,16 @@ for i, (outer_train_index, outer_test_index) in tqdm(enumerate(kf1.split(X))):
     # Normalize val using train
     X_par, d = normalize(X_par)
     X_test = X_test / d
+    
+    # I think we should normalize even though this is not done in the exercises (Maybe the coeffs will handle it if we don't?)
+    #d = np.linalg.norm(y_par, axis=0, ord=2) 
+    #y_par = y_par / d
+    #y_test = y_test / d
+    ###############
 
     # Train optimal model
-    with warnings.catch_warnings(): # disable convergence warnings from elastic net
-        warnings.simplefilter("ignore")
-        model_outer = ElasticNet(alpha=lambda_opt.item(), l1_ratio=alpha_opt.item())
-        model_outer.fit(X_par, y_par)
+    model_outer = Ridge(alpha=lambda_opt, fit_intercept = False)
+    model_outer.fit(X_par, y_par)
     
     # Training predictions
     beta_outer = model_outer.coef_.ravel()
@@ -128,7 +122,6 @@ for i, (outer_train_index, outer_test_index) in tqdm(enumerate(kf1.split(X))):
     # Compute test error
     Err_par[i] = np.sqrt(((y_par - y_hat_par)**2).mean())
     Err_test[i] = np.sqrt(((y_test - y_hat_test)**2).mean())
-    alpha_opts[i] = alpha_opt
     lambda_opts[i] = lambda_opt
 
 # Compute estimate of generalization error
@@ -137,38 +130,39 @@ E_gen = Err_test.mean()
 # Results:
 print('Computed estimated test error E_test[i]: ', Err_test)
 print('')
-print('Correspondind optimal hyperparamter (alpha*, lambda*): ', list(zip(alpha_opts, lambda_opts)))
+print('Correspondind optimal hyperparamter p*: ', lambda_opts)
 print('')
 print('Estimated generalization error: ')
 print(E_gen)
 
-#fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), dpi=100)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), dpi=100)
 
-#ax1.plot(len(alphas), Err_tr.T, ':', alpha=0.5)
-#ax1.plot(Err_tr.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
-#ax1.set_title('Training error for each fold')
-#ax1.legend()
-#ax1.set_xlabel(r"non-zero $\beta$")
-#ax1.set_ylabel('RMSE')
+ax1.plot(lambdas, Err_tr.T, ':', alpha=0.5)
+ax1.plot(lambdas, Err_tr.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
+ax1.set_title('Training error for each fold')
+ax1.legend()
+ax1.set_xlabel(r"$\lambda$")
+ax1.set_ylabel('RMSE')
 
-#ax2.plot(K, Err_val.T, ':', alpha=0.5)
-#ax2.plot(Err_val.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
+ax2.plot(lambdas, Err_val.T, ':', alpha=0.5)
+ax2.plot(lambdas, Err_val.mean(axis=0), c='k', label='Average training error', linewidth=1.5)
 #ax2.axvline(p_opt, label=r"$p^{*}$", linestyle='dashed', c='k', alpha=0.75)
 #[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
-#ax2.set_title('Validation error for each fold')
+ax2.set_title('Validation error for each fold')
 #ax2.set_yscale('log')
-#ax2.legend()
-#ax2.set_xlabel(r"non-zero $\beta$")
-#ax2.set_ylabel('RMSE')
-#plt.show()
+ax2.legend()
+ax2.set_xlabel(r"$\lambda$")
+ax2.set_ylabel('RMSE')
+plt.show()
 
-#fig, ax = plt.subplots(figsize=(15,5), dpi=100)
-#ax.plot(K, Err_tr.mean(axis=0), c='darkblue', label='Average training error')
-#ax.plot(K, Err_val.mean(axis=0), c='firebrick', label='Average validation error')
+fig, ax = plt.subplots(figsize=(15,5), dpi=100)
+ax.plot(lambdas, Err_tr.mean(axis=0), c='darkblue', label='Average training error')
+ax.plot(lambdas, Err_val.mean(axis=0), c='firebrick', label='Average validation error')
 #ax.axvline(p_opt, label=r"$p^{*}$", linestyle='dashed', c='k', alpha=0.75)
-#[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in p_opts]
+#[ax.axvline(_x, linewidth=1, color='k', linestyle='dashed', alpha=0.75) for _x in lambda_opts]
 #ax.set_yscale('log')
-#ax.legend()
-#ax.set_xlabel(r"non-zero $\beta$")
-#ax.set_ylabel('RMSE')
-#plt.show()
+ax.legend()
+ax.set_xlabel(r"$\lambda$")
+ax.set_ylabel('RMSE')
+plt.show()
+